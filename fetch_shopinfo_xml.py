9c349c1e98fb7464a603d5846284f1bee65479ae
Jochen Wersdörfer <jochen@wersdoerfer.de>
2015-08-23 21:30:58 +0200

    cached content fetching + shopinfo parsing

--- fetch_shopinfo_xml.py
+++ fetch_shopinfo_xml.py
@@ -9,10 +9,12 @@ import requests
 from joblib import Memory
 from urllib.parse import unquote
 
-cache = Memory(cachedir='cache')
+from .shopinfo import Shopinfo
+
+memory = Memory(cachedir='cache')
 
 
-def generate_urls(shopcount, blocksize):
+def generate_elmar_urls(shopcount, blocksize):
     url_tmpl = 'http://elektronischer-markt.de/nav?page={}&blocksize={}&dest=search.shoplist'
     pages = int((shopcount / blocksize) + 1)
     for page in range(1, pages + 1):
@@ -30,24 +32,39 @@ def get_shopinfo_url_from_line(line, pattern):
         return url
 
 
-def main(args):
-    #for url in generate_urls(4633, 50):
-    #    print(url)
+@memory.cache
+def get_shopinfo_urls_from_page(elmar_url):
     line_pattern = re.compile('a.target.*counter.*home')
     url_pattern = re.compile(r'redirect=(?P<url>http.*)&amp;rid=2"')
-    with open('page_1', 'r') as content:
-        for line in content:
-            line = line.rstrip()
-            m = line_pattern.search(line)
-            if m is not None:
-                shopinfo_url = get_shopinfo_url_from_line(line, url_pattern)
-                print(shopinfo_url)
-                r = requests.get(shopinfo_url)
-                fname = shopinfo_url.replace('http://', '').replace('/', '_')
-                with open('shopinfo/{}'.format(fname), 'w') as sfile:
-                    sfile.write(r.content)
-                #except Exception:
-                #    print('broken shopinfo: {}'.format(shopinfo_url))
+    shopinfo_urls = []
+    r = requests.get(url)
+    for line in r.content:
+        line = line.rstrip()
+        m = line_pattern.search(line)
+        if m is not None:
+            shopinfo_url = get_shopinfo_url_from_line(line, url_pattern)
+            shopinfo_urls.append(shopinfo_url)
+    return shopinfo_urls
+
+
+@memory.cache
+def get_shopinfo_xml_from_url(shopinfo_url):
+    r = requests.get(shopinfo_url)
+    return r.content
+
+
+def main(args):
+    for elmar_url in generate_elmar_urls(4633, 50):
+        shopinfo_urls = get_shopinfo_urls_from_page(elmar_url)
+        for shopinfo_url in shopinfo_urls:
+            shopinfo_xml = get_shopinfo_xml_form_url(shopinfo_url)
+            shopinfo = Shopinfo(shopinfo_xml)
+        #r = requests.get(url)
+        #fname = shopinfo_url.replace('http://', '').replace('/', '_')
+        #with open('shopinfo/{}'.format(fname), 'w') as sfile:
+        #    sfile.write(r.content)
+        #except Exception:
+        #    print('broken shopinfo: {}'.format(shopinfo_url))
 
 
 if __name__ == '__main__':
---
+++ shopinfo.py
@@ -0,0 +1,47 @@
+import xml.etree.ElementTree as ET
+
+
+class Shopinfo(object):
+    def __init__(self, shopinfo):
+        self.root = ET.fromstring(shopinfo)
+        
+    @property
+    def name(self):
+        return self.root.find('Name').text
+    
+    @property
+    def url(self):
+        return self.root.find('Url').text
+    
+    @property
+    def tabular(self):
+        return self.root.find('.//Tabular')
+    
+    @property    
+    def mappings(self):
+        cols = []
+        mappings = self.tabular.find('Mappings')
+        for mapping in mappings:
+            column_num = int(mapping.attrib['column'])
+            column_name = mapping.attrib['columnName']
+            column_type = mapping.attrib['type']
+            cols.append((column_num, column_name, column_type))
+        return cols
+    
+    @property
+    def csv_url(self):
+        return self.tabular.find('CSV/Url').text
+    
+    @property
+    def product_count(self):
+        return int(self.root.find(".//Categories/TotalProductCount").text)
+    
+    @property
+    def categories(self):
+        categories = []
+        for category in self.root.findall(".//Categories/Item"):
+            name = category.find('Name').text
+            count = category.find('ProductCount').text
+            mapping = category.find('Mapping').text
+            categories.append((name, mapping, int(count)))
+        return categories
