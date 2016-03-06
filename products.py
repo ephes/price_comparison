003cfd541fc40819ed7ba4fd34efff407a668b7e
Jochen Wersdörfer <jochen@wersdoerfer.de>
2016-03-06 20:08:20 +0100

    get products functions

--- products.py
+++ products.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
+from random import sample
+from collections import defaultdict
+
 
 def convert_price(price):
     price = str(price)
@@ -19,9 +22,26 @@ def convert_price(price):
 
 
 def get_products(csv_name='products.csv'):
+    """ get products dataframe from csv"""
     products = pd.read_csv(
         csv_name, error_bad_lines=False, converters={'price': convert_price},
         low_memory=False)
     products = products.drop('Unnamed: 0', 1)
     products = products[~products.price.isnull()]
     return products
+
+
+def get_products_from_different_shops(
+    csv_name='products.csv', n=5, sample_size=10):
+    """ select only eans which appear in more than n shops"""
+    products = get_products(csv_name=csv_name)
+    shops_for_ean = defaultdict(set)
+
+    for ean, shop in zip(products.ean, products.shop):
+        shops_for_ean[ean].add(shop)
+
+    good_eans = list(
+        {ean for ean, shops in shops_for_ean.items() if len(shops) > n})
+    sample_eans = sample(good_eans, sample_size)
+    sample_products = products[products.ean.isin(pd.Series(sample_eans))]
+    return sample_products
